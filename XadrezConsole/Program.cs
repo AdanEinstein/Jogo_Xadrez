1e92016cf82483dff7f0335c1942488092cb469d
AdanEinstein <adaneinstein@gmail.com>
2022-07-10 12:46:18 -0300

    Melhorando a apresentação das pecas no tabuleiro

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -10,20 +10,18 @@ namespace XadrezConsole
         {
             try
             {
-                //Console.WriteLine(new Posicao(3,4));
-                    //Console.WriteLine(new Tabuleiro(8,8));
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 9));
-                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-                Tela.imprimirTabuleiro(tabuleiro);
+                Tela.ImprimirTabuleiro(tab);
+
             }
-            catch (Exception ex)
+            catch (TabuleiroException ex)
             {
                 Console.WriteLine(ex.Message);
-            }
+            }           
         }
     }
 }
--- XadrezConsole/Tela.cs
+++ XadrezConsole/Tela.cs
@@ -1,23 +1,38 @@
 ﻿using System;
-using System.Text;
 using tabuleiro;
 namespace XadrezConsole
 {
     public class Tela
     {
-        public static void imprimirTabuleiro(Tabuleiro tabuleiro)
+        public static void ImprimirTabuleiro(Tabuleiro tabuleiro)
         {
-            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < tabuleiro.Linhas; i++)
             {
+                Console.Write($"{8 - i} ");
                 for (int j = 0; j < tabuleiro.Colunas; j++)
                 {
-                    if (tabuleiro.Peca(i, j) == null) sb.Append("- ");
-                    else sb.Append($"{tabuleiro.Peca(i, j)} ");
+                    if (tabuleiro.Peca(i, j) == null) Console.Write("- ");
+                    else
+                    {
+                        ImprimirPeca(tabuleiro.Peca(i, j));
+                        Console.Write(" ");
+                    }
                 }
-                sb.AppendLine();
+                Console.WriteLine();
             }
-            Console.WriteLine(sb.ToString());
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca) Console.Write(peca);
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
